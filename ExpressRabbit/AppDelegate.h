506aa1a30b039357a05e1e162274a9369707ed7c
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2014-11-23 11:19:27 +0800

    Initial Commit

---
+++ ExpressRabbit/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  ExpressRabbit
+//
+//  Created by kuaiditu on 14-11-23.
+//  Copyright (c) 2014年 kuaiditu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
